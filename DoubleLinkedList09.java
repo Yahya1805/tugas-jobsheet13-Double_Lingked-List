e35d8c036c8746738a0c9b46aaa6673dfc8d6ad6
Yahya1805 <belajarkuliah902@gmail.com>
2025-06-06 16:35:33 +0700

    perubahan untuk latihan and selesai

--- DoubleLinkedList09.java
+++ DoubleLinkedList09.java
@@ -1,10 +1,12 @@
 public class DoubleLinkedList09 {
     private Node09 head;
     private Node09 tail;
+    private int size;
 
     public DoubleLinkedList09() {
         this.head = null;
         this.tail = null;
+        this.size = 0;
     }
 
     boolean isEmpty() {
@@ -20,6 +22,7 @@ public class DoubleLinkedList09 {
             head.prev = newNode;
             head = newNode;
         }
+        size++;
     }
 
     void addLast(Mahasiswa09 data) {
@@ -31,6 +34,32 @@ public class DoubleLinkedList09 {
             newNode.prev = tail;
             tail = newNode;
         }
+        size++;
+    }
+
+    void add(int index, Mahasiswa09 data) {
+        if (index < 0 || index > size) {
+            System.out.println("Index tidak valid.");
+            return;
+        }
+        if (index == 0) {
+            addFirst(data);
+            return;
+        } else if (index == size) {
+            addLast(data);
+            return;
+        }
+
+        Node09 newNode = new Node09(data);
+        Node09 current = head;
+        for (int i = 0; i < index - 1; i++) {
+            current = current.next;
+        }
+        newNode.next = current.next;
+        newNode.prev = current;
+        current.next.prev = newNode;
+        current.next = newNode;
+        size++;
     }
 
     void insertAfter(Mahasiswa09 data, String nim) {
@@ -55,37 +84,36 @@ public class DoubleLinkedList09 {
             current.next.prev = newNode;
             current.next = newNode;
         }
-
+        size++;
         System.out.println("Node dengan NIM " + nim + " telah ditambahkan setelah node tersebut.");
     }
 
-    void print() {
+    public void print() {
         if (isEmpty()) {
-            System.out.println("List kosong.");
-            return;
-        }
-
-        Node09 current = head;
-        while (current != null) {
-            current.data.tampil();
-            current = current.next;
+            System.out.println("Warning: Linked list kosong!");
+        } else {
+            Node09 current = head;
+            while (current != null) {
+                System.out.println("NIM: " + current.data.nim + ", Nama: " + current.data.nama);
+                current = current.next;
+            }
         }
     }
 
     void removeFirst() {
         if (isEmpty()) {
-            System.out.println("List kosong, tidak ada yang dihapus.");
-            return;
-        }
-
-        if (head == tail) {
-            head = tail = null;
+            System.out.println("Linked list kosong, tidak ada data yang dihapus.");
         } else {
+            Mahasiswa09 deletedData = head.data;
             head = head.next;
-            head.prev = null;
+            if (head != null) {
+                head.prev = null;
+            } else {
+                tail = null;
+            }
+            size--;
+            System.out.println("Data sudah berhasil dihapus. Data yang terhapus adalah: NIM = " + deletedData.nim + ", Nama = " + deletedData.nama);
         }
-
-        System.out.println("Node pertama telah dihapus.");
     }
 
     void removeLast() {
@@ -100,8 +128,31 @@ public class DoubleLinkedList09 {
             tail = tail.prev;
             tail.next = null;
         }
-
+        size--;
         System.out.println("Node terakhir telah dihapus.");
+    }
+
+    void remove(int index) {
+        if (index < 0 || index >= size) {
+            System.out.println("Index tidak valid.");
+            return;
+        }
+        if (index == 0) {
+            removeFirst();
+            return;
+        } else if (index == size - 1) {
+            removeLast();
+            return;
+        }
+
+        Node09 current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        current.prev.next = current.next;
+        current.next.prev = current.prev;
+        size--;
+        System.out.println("Node dengan NIM " + current.data.nim + " telah dihapus dari indeks ke-" + index);
     }
 
     void removeAfter(String nim) {
@@ -120,9 +171,41 @@ public class DoubleLinkedList09 {
             } else {
                 tail = current;
             }
-
+            size--;
             System.out.println("Node dengan NIM " + toDelete.data.nim + " telah dihapus.");
         }
+    }
+
+    Mahasiswa09 getFirst() {
+        if (isEmpty()) {
+            System.out.println("Linked list kosong.");
+            return null;
+        }
+        return head.data;
+    }
+
+    Mahasiswa09 getLast() {
+        if (isEmpty()) {
+            System.out.println("Linked list kosong.");
+            return null;
+        }
+        return tail.data;
+    }
+
+    Mahasiswa09 getIndex(int index) {
+        if (index < 0 || index >= size) {
+            System.out.println("Index tidak valid.");
+            return null;
+        }
+        Node09 current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    int getSize() {
+        return size;
     }
 
     Node09 search(String nim) {
